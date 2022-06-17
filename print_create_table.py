2af2642b5ed38b446ffd69da663eddca59d73f33
Yury Kochubeev <kochub@gmail.com>
2022-06-17 18:43:22 +0300

    Creted full pipeline from tracer to clickhouse

---
+++ print_create_table.py
@@ -0,0 +1,22 @@
+
+columns = ['organization_id', 'self', 'id', 'key', 'version', 'pendingReplyFrom',
+    'statusStartTime', 'Effort', 'boards', 'type',
+    'previousStatusLastAssignee', 'createdAt', 'Confidence',
+    'commentWithExternalMessageCount', 'deadline', 'updatedAt',
+    'lastCommentUpdatedAt', 'storyPoints', 'summary', 'Impact', 'Reach',
+    'originalEstimation', 'updatedBy', 'spent', 'start', 'priority',
+    'estimation', 'Score', 'followers', 'createdBy',
+    'commentWithoutExternalMessageCount', 'votes', 'assignee', 'queue',
+    'status', 'previousStatus', 'favorite', 'tags', 'components', 'end',
+    'parent', 'resolvedAt', 'resolvedBy', 'resolution', 'epic', 'sprint',
+    'project', 'sla', 'otvetstvennyj', 'gorod',
+    'otvetstvennyjZaKomandirovki', 'podrazdelenie', 'description', 'cel',
+    'kadrovik', 'sotrudnik', 'buhgalter', 'unique', 'checklistDone',
+    'checklistTotal', 'checklistItems', 'stoimost', 'zatratyrub',
+    'programma', 'votedBy', 'aliases', 'previousQueue', 'emailCreatedBy',
+    'emailTo', 'emailFrom', 'dopolnitelnoeSoglasovanie', 'soglasuusij',
+    'cenaBileta', 'rekruter', 'uhodasijSotrudnik', 'professia',
+    'dataObnovlenia', 'otsutstvie']
+
+for item in columns:
+    print(item+' String,')
